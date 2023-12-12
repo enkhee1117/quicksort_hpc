97cc13050d973ceeb0a6cc80c80f04b5e98ecc80
enkhee1117 <enkhee1117@gmail.com>
2023-12-12 14:22:59 -0600

    from mpi - openmp directives removed

--- quicksort_mpi.cpp
+++ quicksort_mpi.cpp
@@ -62,11 +62,8 @@ int main(int argc, char* argv[]) {
 void quickSort(vector<int>& arr, int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
-#pragma omp parallel sections
         {
-#pragma omp section
             quickSort(arr, low, pi - 1);
-#pragma omp section
             quickSort(arr, pi + 1, high);
         };
     }
